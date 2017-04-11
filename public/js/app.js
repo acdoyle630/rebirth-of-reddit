8d636adf4b57164c5dcee91b6d64f8d903d19b57
Adam <acdoyle630@gmail.com>
2017-04-10 18:47:28 -1000

    page pictures load,  with self pic load reddit alien

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,42 @@
-console.log('test');
+/*jshint esversion: 6*/
+
+var load = document.querySelector('#random');
+
+window.addEventListener('load', snowboard);
+
+
+function snowboard(){
+  oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', updateDisplay);
+  oReq.open('GET', 'http://www.reddit.com/r/chibears.json');
+  oReq.send();
+}
+
+function updateDisplay(){
+  const requestData = JSON.parse(this.responseText);
+  console.log(requestData.data.children);
+  for(var i = 0; i<requestData.data.children.length; i++){
+    if(requestData.data.children[i].data.thumbnail === 'self'){
+      selfPic();
+    } else {
+      thumbPic(requestData.data.children[i].data.thumbnail);
+      }
+    }
+}
+
+function selfPic(){
+  const pic = document.createElement('IMG');
+  pic.setAttribute('src', 'https://cdn.worldvectorlogo.com/logos/reddit-2.svg');
+  pic.setAttribute('width', '300');
+  pic.setAttribute('height', '300');
+  document.body.appendChild(pic);
+}
+
+function thumbPic(picture){
+  const pic = document.createElement('IMG');
+  pic.setAttribute('src', picture);
+  pic.setAttribute('width', '300');
+  pic.setAttribute('height', '300');
+  document.body.appendChild(pic);
+}
+
