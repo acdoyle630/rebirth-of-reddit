a4dcce3cd2567006a609ca61406f9bba95889d43
Adam <acdoyle630@gmail.com>
2017-04-11 15:12:47 -1000

    title and thumbnail in sigle div

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,11 @@
 /*jshint esversion: 6*/
 
-var load = document.querySelector('#random');
+window.addEventListener('load', pageLoad);
 
-window.addEventListener('load', snowboard);
-
-
-function snowboard(){
+function pageLoad(){
   oReq = new XMLHttpRequest();
   oReq.addEventListener('load', updateDisplay);
-  oReq.open('GET', 'http://www.reddit.com/r/gifs.json');
+  oReq.open('GET', 'http://www.reddit.com/r/chibears.json');
   oReq.send();
 }
 
@@ -17,31 +14,51 @@ function updateDisplay(){
   console.log(requestData.data.children);
   for(var i = 0; i<requestData.data.children.length; i++){
     if(requestData.data.children[i].data.thumbnail === 'self'){
-      selfPic();
+      selfPic(requestData.data.children[i].data);
     } else {
-      thumbPic(requestData.data.children[i].data.thumbnail);
+      thumbPic(requestData.data.children[i].data);
       }
     }
 }
 
-function selfPic(){
+function selfPic(picture){
+ //create thumbnail
   const pic = document.createElement('IMG');
   const picFrame = document.createElement('div');
-  picFrame.class = 'content';
+  picFrame.className = 'frameContent';
+
+  //create title
+  const title = document.createElement('p');
+  title.className = 'titleContent';
+  title.innerHTML = picture.title;
+  picFrame.appendChild(title);
+
+  //set thumbnail display
   pic.setAttribute('src', 'https://cdn.worldvectorlogo.com/logos/reddit-2.svg');
-  pic.setAttribute('width', '300');
-  pic.setAttribute('height', '300');
+  pic.setAttribute('width', '100');
+  pic.setAttribute('height', '100');
+  pic.className = 'thumbnailContent';
   picFrame.appendChild(pic);
   homepage.appendChild(picFrame);
 }
 
 function thumbPic(picture){
+  //create Thumbnail
   const pic = document.createElement('IMG');
   const picFrame = document.createElement('div');
-  picFrame.class = 'content';
-  pic.setAttribute('src', picture);
-  pic.setAttribute('width', '300');
-  pic.setAttribute('height', '300');
+  picFrame.className = 'frameContent';
+
+  //create title
+  const title = document.createElement('p');
+  title.className = 'titleContent';
+  title.innerHTML = picture.title;
+  picFrame.appendChild(title);
+
+  //set thumbnail display
+  pic.setAttribute('src', picture.thumbnail);
+  pic.setAttribute('width', '100');
+  pic.setAttribute('height', '100');
+  pic.className = 'thumbnailContent';
   picFrame.appendChild(pic);
   homepage.appendChild(picFrame);
 }
