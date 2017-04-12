9c941e5884cd94f3b40441ff18188cd49798328f
Adam <acdoyle630@gmail.com>
2017-04-11 16:52:06 -1000

    centered content in white divs, added author to data

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,38 @@
 /*jshint esversion: 6*/
 
 window.addEventListener('load', pageLoad);
+let homePage = 'all';
+let myPage = 'snowboarding';
+const random = document.querySelector('#random');
+const myBoards = document.querySelector('#myBoards');
+const app = document.querySelector('#getApp');
+const homeScreen = document.querySelector('#homepage');
+const search = document.querySelector('#searchBar');
+const searchButton = document.querySelector('#searchButton');
+const subreddit = document.querySelector('#subreddit');
+
+searchButton.addEventListener('click', function(){
+//reload page with new homePage
+  homeScreen.innerHTML = '';
+  homePage = search.value;
+  subreddit.innerHTML = homePage;
+  console.log(homePage);
+  pageLoad();
+});
+
+myBoards.addEventListener('click', function(){
+  homeScreen.innerHTML = '';
+  homePage = myPage;
+  subreddit.innerHTML = homePage;
+  console.log(homePage);
+  console.log(myPage);
+  pageLoad();
+});
 
 function pageLoad(){
   oReq = new XMLHttpRequest();
   oReq.addEventListener('load', updateDisplay);
-  oReq.open('GET', 'http://www.reddit.com/r/chibears.json');
+  oReq.open('GET', 'http://www.reddit.com/r/'+ homePage +'.json');
   oReq.send();
 }
 
@@ -35,11 +62,17 @@ function selfPic(picture){
 
   //set thumbnail display
   pic.setAttribute('src', 'https://cdn.worldvectorlogo.com/logos/reddit-2.svg');
-  pic.setAttribute('width', '100');
-  pic.setAttribute('height', '100');
+  pic.setAttribute('width', '200');
+  pic.setAttribute('height', '200');
   pic.className = 'thumbnailContent';
   picFrame.appendChild(pic);
   homepage.appendChild(picFrame);
+
+  //Author
+  const author = document.createElement('h4');
+  author.className = 'author';
+  author.innerHTML = picture.author;
+  title.appendChild(author);
 }
 
 function thumbPic(picture){
@@ -56,10 +89,16 @@ function thumbPic(picture){
 
   //set thumbnail display
   pic.setAttribute('src', picture.thumbnail);
-  pic.setAttribute('width', '100');
-  pic.setAttribute('height', '100');
+  pic.setAttribute('width', '200');
+  pic.setAttribute('height', '200');
   pic.className = 'thumbnailContent';
   picFrame.appendChild(pic);
   homepage.appendChild(picFrame);
+
+  //author
+  const author = document.createElement('h4');
+  author.className = 'author';
+  author.innerHTML = picture.author;
+  title.appendChild(author);
 }
 
